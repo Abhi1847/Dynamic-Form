f89b32b52f211f8e4ef00883d4aa98bf65a1c342
Abhi1847 <abhinprajapati@gmail.com.com>
2024-09-24 17:03:18 +0530

    peram

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import "./assets/style.css";
 import FormLayout from "./component/FormLayout";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <>
-      <FormLayout></FormLayout>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/form/:Name" element={<FormLayout />} />
+
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
   );
 }
 
--- src/component/FormLayout.jsx
+++ src/component/FormLayout.jsx
@@ -18,14 +18,16 @@ import { DemoContainer } from "@mui/x-date-pickers/internals/demo";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DatePicker } from "@mui/x-date-pickers/DatePicker";
+import { useParams } from "react-router-dom";
+import dayjs from "dayjs";
 
 function FormLayout() {
   const [step, setstep] = useState(1);
   const [formdata, setformdata] = useState([]);
   const [fielddata, setfielddata] = useState([]);
-  const [data, setdata] = useState();
+  const [date, setdate] = useState({});
   const [groupList, setgroupList] = useState([]);
-  const [checkdata, setcheckdata] = useState();
+  // const [checkdata, setcheckdata] = useState();
   const [filteredData, setFilteredData] = useState([]);
   const [stepdata, setstepdata] = useState([]);
   const [textFieldData, setTextFieldData] = useState({});
@@ -33,16 +35,39 @@ function FormLayout() {
   const [loading, setloading] = useState(false);
   const [errors, setErrors] = useState({});
   const [currentGroupIndex, setCurrentGroupIndex] = useState(0);
-
+  const { Name } = useParams();
+  const [textAreaValue, setTextAreaValue] = useState({});
+  const [selectedDate, setSelectedDate] = useState(null);
+  const [option, setoption] = useState("");
+  console.log("parameter", Name);
+  //form useeffect
   useEffect(() => {
     const fetchData = async () => {
       try {
         setloading(true);
-        const formresponse = await axios.get("https://c3yl8he1e1.execute-api.us-west-2.amazonaws.com/dev/form");
-        const fieldresponse = await axios.get("https://c3yl8he1e1.execute-api.us-west-2.amazonaws.com/dev/field");
+
+        const formresponse = await axios.get(
+          `https://c3yl8he1e1.execute-api.us-west-2.amazonaws.com/dev/form/${Name}`,
+          // `http://localhost:8000/form/${Name}`,
+          {
+            headers: {
+              "Cache-Control": "no-cache",
+            },
+          }
+        );
+        const fieldresponse = await axios.get(
+          `https://c3yl8he1e1.execute-api.us-west-2.amazonaws.com/dev/field/${Name}`,
+          // `http://localhost:8000/field/${Name}`,
+          {
+            headers: {
+              "Cache-Control": "no-cache",
+            },
+          }
+        );
 
         setformdata(formresponse.data);
         setfielddata(fieldresponse.data);
+
         setloading(false);
       } catch (err) {
         console.log("Error:", err);
@@ -53,6 +78,18 @@ function FormLayout() {
     fetchData();
   }, []);
 
+  //form favicon and title set useeffect
+  useEffect(() => {
+    document.title = formdata.formtitle;
+    const favicon =
+      document.querySelector("link[rel='icon']") ||
+      document.createElement("link");
+    favicon.rel = "icon";
+    favicon.href = formdata.formlogo;
+    document.head.appendChild(favicon);
+  }, [formdata]);
+
+  //finding unique group data
   useEffect(() => {
     if (fielddata?.data) {
       const uniqueGroups = new Set();
@@ -64,9 +101,9 @@ function FormLayout() {
       const uniqueGroupsArray = Array.from(uniqueGroups);
       setgroupList(uniqueGroupsArray);
     }
-
   }, [fielddata]);
 
+  //save step data
   const saveStepData = () => {
     const currentStepData = {
       textFields: textFieldData,
@@ -81,38 +118,64 @@ function FormLayout() {
 
   const validateFields = () => {
     const newErrors = {};
-    fielddata.field?.slice(0, 3).forEach((data) => {
-      if (
+
+    fielddata.field?.forEach((data) => {
+      console.log(
+        "validate...",
+        data.isrequire,
         !textFieldData[data.fieldid] ||
-        textFieldData[data.fieldid].trim() === ""
+          textFieldData[data.fieldid].trim() === ""
+      );
+      if (
+        data.isrequire &&
+        (!textFieldData[data.fieldid] ||
+          textFieldData[data.fieldid].trim() === "")
       ) {
-        newErrors[data.fieldid] = true;
+        newErrors[data.fieldid] = `${data.fieldinfo} is required`;
+      }
+    });
+
+    fielddata.field?.forEach((data) => {
+      if (data.fieldtype === "CheckboxGroup" && data.isRequired) {
+        const checkboxEntry = checkboxData.find(
+          (item) => item.fieldid === data.fieldid
+        );
+        if (
+          !checkboxEntry ||
+          (checkboxEntry.options && checkboxEntry.options.length === 0)
+        ) {
+          newErrors[
+            data.fieldid
+          ] = `${data.fieldtitle} requires at least one selection`;
+        }
       }
     });
     setErrors(newErrors);
-    return Object.keys(newErrors).length === 0; 
+    return Object.keys(newErrors).length === 0;
   };
 
+  //handle next button function
   const handleNext = () => {
     if (validateFields()) {
       saveStepData();
       if (currentGroupIndex < groupList.length - 1) {
-        setCurrentGroupIndex((prevIndex) => prevIndex + 1); 
+        setCurrentGroupIndex((prevIndex) => prevIndex + 1);
       }
     } else {
       console.log("Validation failed, cannot proceed.");
     }
   };
 
+  //handle previous button function
   const handlePrevious = () => {
     saveStepData();
     if (currentGroupIndex > 0) {
-      setCurrentGroupIndex((prevIndex) => prevIndex - 1); 
+      setCurrentGroupIndex((prevIndex) => prevIndex - 1);
     }
   };
 
+  //handle textfield change function
   const handleTextfieldChange = (e, fieldid) => {
-    console.log("text change...", fieldid);
     setTextFieldData({
       ...textFieldData,
       [fieldid]: e.target.value,
@@ -120,36 +183,78 @@ function FormLayout() {
     setErrors({ ...errors, [fieldid]: false });
   };
 
+  //handle checkbox change function
   const handleCheckBoxChange = (e, option, id, fieldid) => {
     setCheckboxData((prevData) => {
       if (e.target.checked) {
-        return [...prevData, { id, option, fieldid }];
-      } else {
-        return prevData.filter(
-          (item) =>
-            !(
-              item.id === id &&
-              item.option === option &&
-              item.fieldid === fieldid
-            )
+        const existingEntry = prevData.find(
+          (item) => item.id === id && item.fieldid === fieldid
         );
+
+        if (existingEntry) {
+          return prevData.map((item) =>
+            item.id === id && item.fieldid === fieldid
+              ? { ...item, options: [...(item.options || []), option] }
+              : item
+          );
+        } else {
+          return [...prevData, { id, fieldid, options: [option] }];
+        }
+      } else {
+        return prevData
+          .map((item) => {
+            if (item.id === id && item.fieldid === fieldid) {
+              const updatedOptions = item.options.filter(
+                (opt) => opt !== option
+              );
+              return updatedOptions.length > 0
+                ? { ...item, options: updatedOptions }
+                : null;
+            }
+            return item;
+          })
+          .filter((item) => item !== null);
       }
     });
   };
 
+  //handle textarea change function
+  const handleTextAreaChange = (e, id, groupid) => {
+    const newValue = e.target.value;
+
+    setTextAreaValue((prevState) => ({
+      ...prevState,
+      [groupid]: {
+        ...prevState[groupid],
+        [id]: { groupid, id, value: newValue },
+      },
+    }));
+  };
+
+  //handle form submit function
   const handleSubmit = async () => {
     const formSubmissionData = {
       textFields: textFieldData,
       checkboxes: checkboxData,
       formid: formdata.formid,
+      Date: date,
+      textAreaValue: textAreaValue,
+      otheroption: option,
     };
     // const response = await axios.post(
-    //   "http://localhost:3000/submit-form",
+    //   "http://localhost:8000/submit-form",
     //   formSubmissionData
     // );
     console.log("response...", formSubmissionData);
+    setTextFieldData({});
+    setCheckboxData([]);
+    setoption("");
+    setdate({});
+    setTextAreaValue({});
+    setSelectedDate(null);
   };
 
+  //save step data in textfield and checkbox
   useEffect(() => {
     const loadStepData = () => {
       const savedData = stepdata[step];
@@ -165,11 +270,37 @@ function FormLayout() {
     loadStepData();
   }, []);
 
+  //filter for option selected as Other
   useEffect(() => {
-    const filtered = checkboxData.filter((item) => item.option === "Other");
+    const filtered = checkboxData.filter((item) =>
+      item.options?.includes("Other")
+    );
     setFilteredData(filtered);
   }, [checkboxData]);
 
+  const handleDateChange = (newValue, id) => {
+    if (newValue) {
+      const formattedDate = dayjs(newValue).format("DD MM YYYY");
+      setdate({ formattedDate, id });
+    }
+    setSelectedDate(newValue);
+  };
+
+  //handle other option change function
+  const handleoptionChange = (e, id, groupid) => {
+    const options = e.target.value;
+
+    setoption((prevOptions) => ({
+      ...prevOptions,
+      [id]: {
+        ...prevOptions[id],
+        options: options,
+        groupid: groupid,
+      },
+    }));
+  };
+
+  //rendering fields
   const renderfield = (field) => {
     const render = field.fieldtype;
     switch (render) {
@@ -177,7 +308,14 @@ function FormLayout() {
         return (
           <Grid item xs={12} sm={6} md={6}>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DatePicker label={field.fieldtitle} sx={{ width: "70%" }} />
+              <DatePicker
+                label={field.fieldtitle}
+                value={selectedDate}
+                onChange={(newValue) =>
+                  handleDateChange(newValue, field.fieldid)
+                }
+                sx={{ width: "70%" }}
+              />
             </LocalizationProvider>
           </Grid>
         );
@@ -200,7 +338,6 @@ function FormLayout() {
             <Typography variant="body1"> {field.fieldinfo}</Typography>
             <Grid container mt={1}>
               {field.fieldoptions?.options?.map((groupdata, idx) => (
-                // console.log("options===", groupdata)
                 <Grid item xs={12} sm={4} key={idx}>
                   <FormControlLabel
                     control={
@@ -208,7 +345,7 @@ function FormLayout() {
                         checked={checkboxData.some(
                           (data) =>
                             data.id === field.groupid &&
-                            data.option === groupdata &&
+                            data.options.includes(groupdata) &&
                             data.fieldid === field.fieldid
                         )}
                         onChange={(e) =>
@@ -227,15 +364,15 @@ function FormLayout() {
               ))}
             </Grid>
             {filteredData.some((item) => item.fieldid === field.fieldid) && (
-              // if(filteredData.fieldid === field.fieldid ){
               <Grid item xs={12}>
                 <TextField
                   fullWidth
                   variant="outlined"
                   sx={{ mb: 2 }}
-                  value={textFieldData[field.fieldid] || ""}
-                  onChange={(e) => handleTextfieldChange(e, field.fieldid)}
-                  // disabled="true"
+                  value={option[field.fieldid]?.options || ""}
+                  onChange={(e) =>
+                    handleoptionChange(e, field.fieldid, field.groupid)
+                  }
                 />
               </Grid>
             )}
@@ -246,12 +383,17 @@ function FormLayout() {
           <Grid container justifyContent="space-between" mb={2}>
             <Typography variant="body1">{field.fieldinfo}</Typography>
 
-            <TextField
-              fullWidth
-              multiline
+            <Textarea
+              name="Outlined"
+              placeholder=""
+              variant="outlined"
+              sx={{ width: "100%" }}
+              // maxRows={4}
+              value={textAreaValue[field.groupid]?.[field.fieldid]?.value || ""}
               rows={4}
-              // value={textFieldData[data.fieldid] || ""}
-              // onChange={(e) => handleTextfieldChange(e, data.fieldid)}
+              onChange={(e) =>
+                handleTextAreaChange(e, field.fieldid, field.groupid)
+              }
             />
           </Grid>
         );
@@ -260,14 +402,9 @@ function FormLayout() {
     }
   };
 
+  //rendering field withgroup
   const renderFieldsWithGroup = () => {
-    // const renderedGroups = new Set();
     const currentGroupTitle = groupList[currentGroupIndex];
-
-    // return fielddata?.data?.map((field) => {
-    //   const groupid = field.groupid;
-    //   const grouptitle = field.grouptitle;
-
     return (
       <Box>
         <Typography
@@ -279,11 +416,7 @@ function FormLayout() {
         >
           {currentGroupTitle}
         </Typography>
-        {/* {renderGroupTitle(groupid, grouptitle, renderedGroups)}
-          {renderfield(field)} */}
         <Grid container>
-          {/* Render fields based on the current group */}
-
           {fielddata?.data
             ?.filter((field) => field.grouptitle === currentGroupTitle)
             .map((field) => (
@@ -295,6 +428,7 @@ function FormLayout() {
       </Box>
     );
   };
+
   return (
     <>
       {loading ? (
