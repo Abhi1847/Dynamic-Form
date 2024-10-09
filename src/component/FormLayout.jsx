cdc880966fdfd21b1551884f16b1b80a787d8a09
Abhi1847 <abhinprajapati@gmail.com.com>
2024-10-09 15:20:25 +0530

    code change

--- src/component/FormLayout.jsx
+++ src/component/FormLayout.jsx
@@ -459,6 +459,7 @@ function FormLayout() {
         // "http://localhost:8000/submit-form",
         formSubmissionData
       );
+      console.log("hii");
       // handledemo(formSubmissionData);
       const pdfBlob = await handlepdf(formSubmissionData);
 
